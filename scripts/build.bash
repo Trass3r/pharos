607c22d4a73986334f652e5f35dc6180eefd5488
Trass3r <un@known.com>
2019-10-03 13:02:17 +0200

    try LTO

--- scripts/build.bash
+++ scripts/build.bash
@@ -15,6 +15,8 @@ sudo ldconfig
 test -d build && rm -rf build
 mkdir build
 cd build
+CXXFLAGS='-O3 -flto -march=haswell' \
+LDFLAGS='-O3 -flto -march=haswell' \
 cmake -GNinja -DCMAKE_INSTALL_PREFIX=$PREFIX -DROSE_ROOT=$PREFIX \
       -DBOOST_ROOT=$PREFIX -DZ3_ROOT=$PREFIX -DSWIPL_ROOT=$PREFIX \
       -DYamlCpp_ROOT=/usr ..
